a1862dfdc7c2777499e15ad3ab2fe1a97fe17031
Gabriel Cruz <10182157+gabrielthecruz@users.noreply.github.com>
2025-11-23 21:54:54 -0300

    Update emacs.custom.el

--- .emacs.d/emacs.custom.el
+++ .emacs.d/emacs.custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
-     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
      default))
  '(package-selected-packages
-   '(company dap-mode gruber-darker-theme ido-vertical-mode lsp-ui smex)))
+   '(company dap-mode gruber-darker-theme ido-vertical-mode org-roam
+	     org-roam-ui smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
