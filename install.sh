f3e6025c69b6e5518f196a2e1c5d78142ef78db0
Gabriel Cruz <gabriel.cruz232@gmail.com>
2025-11-26 14:02:43 -0300

    install script for what i need

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pacman -S emacs
+pacman -S pipewire wireplumber xdg-desktop-portal-hyprland dunst
+pacman -S ttf-jetbrains-mono-nerd
+pacman -S hyprland hyprlock hyprshot
+pacman -S waybar rofi rofi-bluetooth
+pacman -S gimp
+pacman -S kitty
+pacman -S piper # mouse config
+pacman -S pavucontrol
+
+
+flatpak install spotify
+flatpak install bitwarden
