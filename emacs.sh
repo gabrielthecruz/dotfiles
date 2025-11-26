cfe59826dd2d1f7dbce2f87a5816d1284944b604
Gabriel Cruz <gabriel.cruz232@gmail.com>
2025-11-26 00:51:13 -0300

    updated emacs configs

---
+++ emacs.sh
@@ -0,0 +1,12 @@
+# packages to install on emacs
+# company
+# dap-mode
+# gruber-darker-theme
+#   ^ M-x > customize-themes to set as theme
+# ido-vertical-mode
+# org-roam
+# org-roam-ui
+# smex
+
+cp .emacs.d/ ~/
+touch ~/.emacs.d/emacs.custom.el
